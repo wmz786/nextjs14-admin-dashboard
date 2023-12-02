213149b9482b57ae4ea851f76b146062748ee271
wmz786 <waismalikzada9@gmail.com>
2023-12-02 11:46:39 +0430

    login page and styles

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,5 +1,16 @@
+import styles from "@/app/ui/login/login.module.css";
+
 const LoginPage = () => {
-  return <div>LoginPage page</div>;
+  return (
+    <div className={styles.container}>
+      <form action="" className={styles.form}>
+        <h1>Login</h1>
+        <input type="text" name="" placeholder="username" />
+        <input type="password" name="" placeholder="password" />
+        <button>Login</button>
+      </form>
+    </div>
+  );
 };
 
 export default LoginPage;
