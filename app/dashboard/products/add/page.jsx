af94068d45cd1b7d0bf6770e7b029e5d24a77ff4
wmz786 <waismalikzada9@gmail.com>
2023-12-02 10:59:31 +0430

    addProduct and styles

---
+++ app/dashboard/products/add/page.jsx
@@ -0,0 +1,30 @@
+import styles from "@/app/ui/dashboard/products/addProduct/addProduct.module.css";
+
+const AddProductsPage = () => {
+  return (
+    <div className={styles.container}>
+      <form action="" className={styles.form}>
+        <input type="text" name="title" placeholder="title" required />
+        <select name="cat" id="cat">
+          <option value="general">Choose a Category</option>
+          <option value="kitchen">kitchen</option>
+          <option value="phone">Phone</option>
+          <option value="computer">Computer</option>
+        </select>
+        <input type="number" name="price" placeholder="price" />
+        <input type="number" name="stock" placeholder="stock" />
+        <input type="text" name="color" placeholder="color" />
+        <input type="text" name="size" placeholder="size" />
+        <textarea
+          name="desc"
+          id="desc"
+          rows="16"
+          placeholder="Description"
+        ></textarea>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default AddProductsPage;
