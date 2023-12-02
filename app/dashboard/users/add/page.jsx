90edcc8c29fc7c07a9314df4fdd326840951cb51
wmz786 <waismalikzada9@gmail.com>
2023-12-02 11:00:22 +0430

    addUser and styles

---
+++ app/dashboard/users/add/page.jsx
@@ -0,0 +1,42 @@
+import styles from "@/app/ui/dashboard/products/addProduct/addProduct.module.css";
+
+const AddUserPage = () => {
+  return (
+    <div className={styles.container}>
+      <form action="" className={styles.form}>
+        <input type="text" name="username" placeholder="username" required />
+        <input type="email" name="email" placeholder="email" required />
+        <input
+          type="password"
+          name="password"
+          placeholder="password"
+          required
+        />
+        <input type="phone" name="phone" placeholder="phone" />
+        <select name="isAdmin" id="isAdmin">
+          <option value={false} selected>
+            Is Admin
+          </option>
+          <option value={true}>Yes</option>
+          <option value={false}>No</option>
+        </select>
+        <select name="isActive" id="isActive">
+          <option value={true} selected>
+            Is Active?
+          </option>
+          <option value={true}>Yes</option>
+          <option value={false}>No</option>
+        </select>
+        <textarea
+          name="address"
+          id="address"
+          rows="16"
+          placeholder="Address"
+        ></textarea>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default AddUserPage;
