be1e97b5cd8d8af404f4eb738670235e21a3cd48
wmz786 <waismalikzada9@gmail.com>
2023-12-03 14:47:39 +0430

    hash password using bcrypt package

--- app/lib/actions.js
+++ app/lib/actions.js
@@ -2,12 +2,15 @@ import { User } from "./models";
 import { connectToDB } from "./utils";
 import { redirect } from "next/navigation";
 import { revalidatePath } from "next/cache";
+import bcrypt from "bcrypt";
 
 export const addUser = async (formData) => {
   "use server";
   const data = Object.fromEntries(formData);
   try {
     connectToDB();
+    const salt = await bcrypt.genSalt(10);
+    data.password = await bcrypt.hash(data.password, salt);
     const newUser = new User(data);
     await newUser.save();
   } catch (error) {
