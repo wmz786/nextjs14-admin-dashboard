72cda729b9191baaebf26dd0a8e9e4283c59fdf7
wmz786 <waismalikzada9@gmail.com>
2023-12-04 11:40:33 +0430

    add additional info to session

--- app/auth.js
+++ app/auth.js
@@ -36,4 +36,21 @@ export const { signIn, signOut, auth } = NextAuth({
       },
     }),
   ],
+  // ADD ADDITIONAL INFORMATION TO SESSION
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.username = user.username;
+        token.img = user.img;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (token) {
+        session.user.username = token.username;
+        session.user.img = token.img;
+      }
+      return session;
+    },
+  },
 });
